699b4b18051ffe1dd3a380693d327c2ed6218ed7
chamoddissanayake <dissanayakechamod@gmail.com>
2020-04-05 10:18:00 +0530

    Login Form handle Input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,91 +1,70 @@
 import React from 'react';
 
-    import { login } from '../repository';
+import { login } from '../repository';
+
+export default class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = { username: '', password: '' };
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+  }
+
+  handleUsernameChange(event) {
+    this.setState({
+      username: event.target.value
+    }, () => {
+      console.log("Entered Username: ", this.state.username);
+    });
+  }
+  handlePasswordChange(event) {
+    this.setState({
+      password: event.target.value
+    }, () => {
+      console.log("Entered Password: ", this.state.password);
+    });
+  }
 
 
+  submitLogin = (event) => {
+    event.preventDefault();
+    login(this.state)
+      .then(token => window.location = '/').catch(err => console.log(err));
+  }
 
-    export default class Login extends React.Component{
-
-      constructor() {
-
-        super();
-
-        this.state = { name: '', password: '' };
-
-      }
-
-
-
-      handleInputChange = (event) => 
-
-                this.setState({[event.target.name]: event.target.value})
-
-
-
-      submitLogin = (event) => {
-
-        event.preventDefault();
-
-        login(this.state)
-
-          .then(token => window.location = '/').catch(err => console.log(err));
-
-      }
-
-
-
-      render() {
-
-         return (
-
-          <div className="container">
-
-          <hr/>
-
-            <div className="col-sm-8 col-sm-offset-2">
-
-              <div className="panel panel-primary">
-
-                <div className="panel-heading"><h3>Log in </h3></div>
-
-                <div className="panel-body">
-
-                  <form onSubmit={this.submitLogin}>
-
-                    <div className="form-group">
-
-                      <label>Name:</label>
-
-                      <input type="text" className="form-control" 
-
-                          name="name" onChange={this.handleInputChange}/>
-
-                    </div>
-
-                    <div className="form-group">
-
-                      <label>Password:</label>
-
-                      <input type="password" className="form-control" 
-
-                          name="password" onChange={this.handleInputChange}/>
-
-                    </div>
-
-                    <button type="submit" className="btn btn-success">Submit</button>
-
-                  </form>
-
+  render() {
+    return (
+      <div className="container">
+        <hr />
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="panel panel-primary">
+            <div className="panel-heading"><h3>Log in </h3></div>
+            <div className="panel-body">
+              <form onSubmit={this.submitLogin}>
+                <div className="form-group">
+                  <label>Name:</label>
+                  <input type="text" className="form-control"
+                    name="name" onChange={this.handleUsernameChange} />
                 </div>
-
-              </div>
-
+                <div className="form-group">
+                  <label>Password:</label>
+                  <input type="password" className="form-control"
+                    name="password" onChange={this.handlePasswordChange} />
+                </div>
+                <button type="submit" className="btn btn-success">Submit</button>
+              </form>
             </div>
-
           </div>
+        </div>
+      </div>
 
-        );
+    );
+  }
 
-      }
-
-    }
+}
