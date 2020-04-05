60a53c2c0de5403138c26c4147cd8ea6b2933b1d
Piyumi14 <lbpmdayarathna@gmail.com>
2020-04-05 22:59:27 +0530

    Add Table to View Items page

--- src/components/viewItems.jsx
+++ src/components/viewItems.jsx
@@ -1,22 +1,75 @@
 import React, { Component } from 'react'
+
 import axios from 'axios';
+
+import ProductItem from './ProductItem';
+
+import { getProducts } from '../repository';
+
+import { Link } from 'react-router-dom';
+
 const BASE_URL = 'http://localhost:5000';
+
 export default class viewItems extends Component{
 
     
+    constructor(props) {
+
+        super(props);
+    
+        this.state = {
+    
+          products: []
+    
+        }
+    
+      }
+
+      componentDidMount() {
+
+        getProducts().then((products) => this.setState({ products }));
+    
+      }
+  
 
 
 
+      render() {
 
-    render() {
+        const { products } = this.state;
+    
         return (
-            <div>
-                <h1>View</h1>
-
-
-
-            </div>
-        )
-    }
+    
+          <div className=" container">
+    
+            <h3 className="card-title">View Items</h3><hr />
+            <table className="table table-striped" style={{ marginTop: 20 }}>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Available Quantity</th>
+                <th colSpan="2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              
+            </tbody>
+          </table>
+            
+    
+            <hr />
+    
+            
+    
+    
+    
+          </div>
+    
+        );
+    
+      }
 
 }
