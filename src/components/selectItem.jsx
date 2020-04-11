932e35763d50dc59d0175f65fb1df254b94f6506
chamoddissanayake <dissanayakechamod@gmail.com>
2020-04-11 14:39:09 +0530

    post to server.js

--- src/components/selectItem.jsx
+++ src/components/selectItem.jsx
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
 
+import { postSelectedProduct } from '../repository';
+
 export default class selectItem extends Component {
+
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            currentID: this.props.match.params.id,
+            product: []
+
+        }
+
+    }
+    componentDidMount() {
+
+        postSelectedProduct(this.state.currentID).then((product) => this.setState({ product }));
+
+    }
+
+
+
     render() {
+
+        const { product } = this.state;
+
         return (
             <div>
                 SelectItem
