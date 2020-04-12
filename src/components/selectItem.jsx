93a9d12ff7503a32be005429d4f68a33a5cfb90d
chamoddissanayake <dissanayakechamod@gmail.com>
2020-04-12 12:46:23 +0530

    Item view details Added

--- src/components/selectItem.jsx
+++ src/components/selectItem.jsx
@@ -45,7 +45,11 @@ export default class selectItem extends Component {
 
 
         const sliderCaroselStyle = {
-            width: "20%"
+            width: "100%"
+        };
+
+        const sliderContainerStyle = {
+            width: "100%"
         };
 
         return (
@@ -66,48 +70,130 @@ export default class selectItem extends Component {
                 {selectedProduct.imageURL_2}
                 {selectedProduct.imageURL_3} */}
 
-                {/* //Slider - start */}
-                <div class="container">
-                    <h2>Carousel Example</h2>
-                    <div id="myCarousel" class="carousel slide" data-ride="carousel" style={sliderCaroselStyle} >
+                <h2>{selectedProduct.name}</h2>
+                <table>
+                    <tr>
+                        <td>
+                            {/* Large table left side - start */}
+                            {/* //Slider - start */}
+                            <div class="container" style={sliderContainerStyle}>
 
-                        <ol class="carousel-indicators">
-                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                            <li data-target="#myCarousel" data-slide-to="1"></li>
-                            <li data-target="#myCarousel" data-slide-to="2"></li>
-                            <li data-target="#myCarousel" data-slide-to="3"></li>
-                        </ol>
+                                <div id="myCarousel" class="carousel slide" data-ride="carousel" style={sliderCaroselStyle} >
+
+                                    <ol class="carousel-indicators">
+                                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                                    </ol>
 
 
-                        <div class="carousel-inner">
-                            <div class="item active">
-                                <img src={selectedProduct.imageURL_main} alt="Image Not Found" style={sliderImagestyle} />
+                                    <div class="carousel-inner">
+                                        <div class="item active">
+                                            <img src={selectedProduct.imageURL_main} alt="Image Not Found" style={sliderImagestyle} />
+                                        </div>
+
+                                        <div class="item">
+                                            <img src={selectedProduct.imageURL_1} alt="Image Not Found" style={sliderImagestyle} />
+                                        </div>
+
+                                        <div class="item">
+                                            <img src={selectedProduct.imageURL_2} alt="Image Not Found" style={sliderImagestyle} />
+                                        </div>
+
+                                        <div class="item">
+                                            <img src={selectedProduct.imageURL_3} alt="Image Not Found" style={sliderImagestyle} />
+                                        </div>
+                                    </div>
+
+                                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                        <span class="glyphicon glyphicon-chevron-left"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                        <span class="glyphicon glyphicon-chevron-right"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
                             </div>
+                            {/* Slider - End */}
+                            {/* Large table left side - End */}
+                        </td>
+                        <td>
+                            {/* Large table right side - start */}
 
-                            <div class="item">
-                                <img src={selectedProduct.imageURL_1} alt="Image Not Found" style={sliderImagestyle} />
+                            {/* Details - start */}
+                            <div id="detailsDiv">
+
+                                <table>
+                                    <tr>
+                                        <td>
+                                            Product Category :
+                            </td>
+                                        <td>
+                                            {selectedProduct.category}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Product Brand :
+                            </td>
+                                        <td>
+                                            {selectedProduct.brand}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Product Name :
+                            </td>
+                                        <td>
+                                            {selectedProduct.name}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Product Description :
+                            </td>
+                                        <td>
+                                            {selectedProduct.description}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Product Price :
+                            </td>
+                                        <td>
+                                            {selectedProduct.price}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Available Quantity :
+                            </td>
+                                        <td>
+                                            {selectedProduct.available_quantity}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                        </td>
+                                        <td>
+                                            {/* Add to cart button - Start */}
+                                            {/* Add to cart button - End */}
+                                        </td>
+                                    </tr>
+
+                                </table>
+
                             </div>
+                            {/* Details - End */}
+                            {/* Large table right side - end */}
+                        </td>
+                    </tr>
+                </table>
 
-                            <div class="item">
-                                <img src={selectedProduct.imageURL_2} alt="Image Not Found" style={sliderImagestyle} />
-                            </div>
 
-                            <div class="item">
-                                <img src={selectedProduct.imageURL_3} alt="Image Not Found" style={sliderImagestyle} />
-                            </div>
-                        </div>
 
-                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
-                </div>
-                {/* Slider - End */}
 
 
 
