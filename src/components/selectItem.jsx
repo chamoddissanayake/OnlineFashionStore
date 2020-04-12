16fe19a13fa806147c578914a2aa820a6a06f001
chamoddissanayake <dissanayakechamod@gmail.com>
2020-04-12 12:27:09 +0530

    Image Slider Added

--- src/components/selectItem.jsx
+++ src/components/selectItem.jsx
@@ -38,6 +38,16 @@ export default class selectItem extends Component {
 
         const { selectedProduct } = this.state;
 
+        const sliderImagestyle = {
+            height: "300px",
+
+        };
+
+
+        const sliderCaroselStyle = {
+            width: "20%"
+        };
+
         return (
             <div>
                 SelectItem
@@ -51,10 +61,58 @@ export default class selectItem extends Component {
                 {selectedProduct.description}
                 {selectedProduct.price}
                 {selectedProduct.available_quantity}
-                {selectedProduct.imageURL_main}
+                {/* {selectedProduct.imageURL_main}
                 {selectedProduct.imageURL_1}
                 {selectedProduct.imageURL_2}
-                {selectedProduct.imageURL_3}
+                {selectedProduct.imageURL_3} */}
+
+                {/* //Slider - start */}
+                <div class="container">
+                    <h2>Carousel Example</h2>
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel" style={sliderCaroselStyle} >
+
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                            <li data-target="#myCarousel" data-slide-to="3"></li>
+                        </ol>
+
+
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img src={selectedProduct.imageURL_main} alt="Image Not Found" style={sliderImagestyle} />
+                            </div>
+
+                            <div class="item">
+                                <img src={selectedProduct.imageURL_1} alt="Image Not Found" style={sliderImagestyle} />
+                            </div>
+
+                            <div class="item">
+                                <img src={selectedProduct.imageURL_2} alt="Image Not Found" style={sliderImagestyle} />
+                            </div>
+
+                            <div class="item">
+                                <img src={selectedProduct.imageURL_3} alt="Image Not Found" style={sliderImagestyle} />
+                            </div>
+                        </div>
+
+                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+                {/* Slider - End */}
+
+
+
+
+
 
             </div>
         )
