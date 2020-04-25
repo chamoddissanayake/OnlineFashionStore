d591e17827fc653f4e3e1c97565c679e1f3dc6c7
prageeth2 <prageeth.456>
2020-04-25 18:07:32 +0530

    edit2 Admin Pannel interface

--- src/Admin/admin.js
+++ src/Admin/admin.js
@@ -1,8 +1,16 @@
-angular.module('admin', []).controller('adminCtrl', function($scope ) {
-    $scope.getVal = function() {
-        console.log($scope.updatedInputBox);
+angular.module('admin', []).controller('adminCtrl', function($scope , $interval ) {
 
+    var tick = function() {
+        $scope.clock = Date.now();
     }
+    tick();
+    $interval(tick, 1000);
 
+
+    var date = new Date();
+    var TodaydateArray = [];
+
+    TodaydateArray =  date.toString().split(' ');
+    $scope.displayTodayDate = "     " + TodaydateArray[1] + "/" + TodaydateArray[2] + "/" + TodaydateArray[3] + "\t" + "\t";
 
 });
