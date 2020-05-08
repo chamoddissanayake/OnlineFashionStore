b7c018f450255fc8b0e9b6602f90b9a6cbf93d32
chamoddissanayake <dissanayakechamod@gmail.com>
2020-05-09 00:13:13 +0530

    Cart Modified

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PaymentMethod from './paymentMethod';
+import commentBoxStyles from '../css/cartStyles.css';
 // import { Router } from 'express';
 
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
@@ -101,45 +102,154 @@ export default class Cart extends Component {
                     <div>
 
                         {console.log(this.state.allCartItems)}
-                                Cart
 
-                                <button onClick={this.gotoPaymentMethod}>Proceed >>></button>
+                        <h1 class="CartTitle">Cart</h1>
+
+
 
                     </div>
                 </Router>
 
                 {this.state.allCartItems ? this.state.allCartItems.map(cartItem => (
                     <tr>
-                        {/* <td>{cartItem}</td> */}
-                        <li>{cartItem._id}</li>
-                        <li>{cartItem.id}</li>
-                        <li>{cartItem.category}</li>
-                        <li>{cartItem.brand}</li>
-                        <li>{cartItem.description}</li>
-                        <li>{cartItem.name}</li>
-                        <li>{cartItem.price}</li>
-                        <li>{cartItem.available_quantity}</li>
+                        {/* <li>{cartItem._id}</li> */}
 
-                        <img src={cartItem.imageURL_main} alt="Image missing" height="250" width="200" />
 
-                        <input id="needCount" type="number" value={cartItem.needQuantity} min="0" max={cartItem.available_quantity}
-                            onChange={(e) => {
-                                console.log("sdfsdf" + e.target.value)
-                                cartItem.needQuantity = e.target.value
 
-                                this.updatecart(cartItem);
+                        {/* <li>{cartItem.description}</li> */}
 
-                                this.setState({
-                                })
-                            }}></input>
 
-                        <button onClick={() => this.deleteFromCart(cartItem)} >X</button>
-                        <br></br>
+
+
+
+
+
+
+
+
+
+                        <table className="table-row">
+                            <tr >
+                                <td>
+                                    {/* Left side start  */}
+                                    <img src={cartItem.imageURL_main} alt="Image missing" height="250" width="200" class="cartImage" />
+                                    {/* Left side end  */}
+                                </td>
+                                <td>
+                                    {/* Right side start */}
+
+                                    <table class="smallTable">
+                                        <tr>
+                                            <td>
+
+                                            </td>
+                                            <td>
+                                                <button id="delbutton" type="button" id="delbutton" class="btn btn-outline-danger" onClick={() => this.deleteFromCart(cartItem)} >X</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <label>Item ID</label>
+                                            </td>
+                                            <td>
+                                                <label>{cartItem.id}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <label>Item Category</label>
+                                            </td>
+                                            <td>
+                                                <label> {cartItem.category}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <label>Brand</label>
+                                            </td>
+                                            <td>
+                                                <label>{cartItem.brand}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <label>Name</label>
+                                            </td>
+                                            <td>
+                                                <label>{cartItem.name}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <label>Available Quantity</label>
+                                            </td>
+                                            <td>
+                                                <label>{cartItem.available_quantity}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <label>Item Price</label>
+                                            </td>
+                                            <td>
+                                                <label>{cartItem.price}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <label>Change Quantity</label>
+                                            </td>
+                                            <td>
+
+                                                <input id="needCount" type="number" value={cartItem.needQuantity} min="0" max={cartItem.available_quantity} class="form-control"
+                                                    onChange={(e) => {
+                                                        console.log("sdfsdf" + e.target.value)
+                                                        cartItem.needQuantity = e.target.value
+
+                                                        this.updatecart(cartItem);
+
+                                                        this.setState({
+                                                        })
+                                                    }}></input>
+
+                                            </td>
+                                        </tr>
+
+                                    </table>
+
+                                    {/* Right side end */}
+                                </td>
+                            </tr>
+
+                            <br></br>
+                        </table>
+
+
+
                     </tr>
                 )) : null}
 
 
-                {this.state.totprice}
+
+                <table>
+                    <tr >
+                        <td class="totPriceLbl">
+                            <h3>Your Total Price</h3>
+                        </td>
+                        <td class="totPriceVal">
+                            <h2>{this.state.totprice}</h2>
+                        </td>
+                    </tr>
+                    <tr >
+                        <td class="totPriceLbl">
+                        </td>
+                        <td class="totPriceVal">
+                            {/* pay button */}
+                            <button type="button" class="btn btn-primary" onClick={this.gotoPaymentMethod}>Proceed >>></button>
+                        </td>
+                    </tr>
+                </table>
+
 
             </div>
         )
