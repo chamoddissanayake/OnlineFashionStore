481676b55d0ea84bf1a97170119dea4103566c78
chamoddissanayake <dissanayakechamod@gmail.com>
2020-05-10 00:51:35 +0530

    Wishlist modified

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -108,35 +108,118 @@ export default class Wishlist extends Component {
 
                 {this.state.wishlistArr.map(wishlistItem => (
                     <div>
-                        <li>{wishlistItem._id}</li>
+                        {/* <li>{wishlistItem._id}</li> */}
+
+                        <table>
 
 
-                        <li>{wishlistItem.available_quantity}</li>
-                        <li>{wishlistItem.brand}</li>
-                        <li>{wishlistItem.category}</li>
-                        <li> {wishlistItem.description}</li>
-                        <li> {wishlistItem.id}</li>
-                        <li>  {wishlistItem.name}</li>
-                        <li> {wishlistItem.price}</li>
 
-                        <img src={wishlistItem.imageURL_main} alt="Image missing" height="250" width="200" />
-                        <button type="button" class="btn btn-outline-danger" onClick={() => this.deleteFromWishlist(wishlistItem)} >X</button>
+                            <tr class="table-row">
+                                <td>
+                                    {/* Left column start */}
+                                    <img src={wishlistItem.imageURL_main} alt="Image missing" height="250" width="200" class="imgclass" />
+                                    {/* Left column end */}
+                                </td>
+                                <td class="middle-td-large">
+                                    {/* Middle column start */}
+                                    <table class="middle-tbl">
+                                        <tr class="middle-tbl-row">
+                                            <td class="td-lbl">
+                                                <label>Item ID</label>
+                                            </td>
+                                            <td class="td-val">
+                                                <label>{wishlistItem.id}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="td-lbl">
+                                                <label>Category</label>
+                                            </td>
+                                            <td class="td-val">
+                                                <label>{wishlistItem.category}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="td-lbl">
+                                                <label>Brand</label>
+                                            </td>
+                                            <td class="td-val">
+                                                <label>{wishlistItem.brand}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="td-lbl">
+                                                <label>Name</label>
+                                            </td>
+                                            <td class="td-val">
+                                                <label>{wishlistItem.name}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="td-lbl">
+                                                <label>Description</label>
+                                            </td>
+                                            <td class="td-val">
+                                                <label>{wishlistItem.description}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="td-lbl">
+                                                <label>Available Quantity</label>
+                                            </td>
+                                            <td class="td-val">
+                                                <label>{wishlistItem.available_quantity}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="td-lbl">
+                                                <label>Price</label>
+                                            </td>
+                                            <td class="td-val">
+                                                <label>{wishlistItem.price}</label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="td-lbl">
+                                                <label>Need Amount</label>
+                                            </td>
+                                            <td class="td-val">
+                                                <input id="needCount" type="number" value={wishlistItem.needQuantity ? wishlistItem.needQuantity : (wishlistItem.needQuantity = 1)} min="0" max={wishlistItem.available_quantity}
+                                                    onChange={(e) => {
+                                                        // console.log("sdfsdf" + e.target.value)
+                                                        wishlistItem.needQuantity = e.target.value
 
-                        <input id="needCount" type="number" value={wishlistItem.needQuantity ? wishlistItem.needQuantity : (wishlistItem.needQuantity = 1)} min="0" max={wishlistItem.available_quantity}
-                            onChange={(e) => {
-                                // console.log("sdfsdf" + e.target.value)
-                                wishlistItem.needQuantity = e.target.value
+                                                        // this.updatecart(cartItem);
 
-                                // this.updatecart(cartItem);
+                                                        this.setState({
+                                                        })
+                                                    }}></input>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    {/* Middle column end */}
+                                </td>
+                                <td class="addToCartCol">
+                                    {/* Right column start */}
+                                    <button className="btn btn-sm btn-warning float-right"
 
-                                this.setState({
-                                })
-                            }}></input>
+                                        onClick={() => { this.addToCart(wishlistItem) }}>Add to cart</button>
+                                    {/* Right column end */}
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-outline-danger" id="deletebtn" onClick={() => this.deleteFromWishlist(wishlistItem)} >X</button>
+                                </td>
+                            </tr>
+                        </table>
 
 
-                        <button className="btn btn-sm btn-warning float-right"
 
-                            onClick={() => { this.addToCart(wishlistItem) }}>Add to cart</button>
+
+
+
+
+
+
 
 
                         <hr />
