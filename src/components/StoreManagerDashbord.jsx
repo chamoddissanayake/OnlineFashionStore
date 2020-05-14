3197fc18b6348a2a815b5169d8a060945518a9a8
Piyumi14 <lbpmdayarathna@gmail.com>
2020-05-14 23:31:33 +0530

    Add images to store manager dashbord

--- src/components/StoreManagerDashbord.jsx
+++ src/components/StoreManagerDashbord.jsx
@@ -12,22 +12,30 @@ export default class StoreManagerDashbord extends Component {
     render() {
         return (
             <div >
-                <h1>Store Manager - Dashbord</h1><br></br><br></br><br></br><br></br>
+                <center>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSZXwCx8bxTtm2LXkzAjX-6hhmMHAw2w38O-sjOXeY3Y0ngzhFO&usqp=CAU" ></img>
+                <br></br><br></br><br></br><br></br><br></br><br></br>
 
+                <img src="https://lh3.googleusercontent.com/proxy/GvOhCQOo00tj6SQnZ9hT00BrdiBhK6VRiDGfag-OKiVKQ_Wr-uJ_YkelW8fWCJ4Aex4Wy4k0KXlC3wa9zOlecfdRjEgmiP53Ld3_EaVI9sdz" height="100" width="120"></img>
+              
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTl_nI8QXoOxGLACnp8dM4Ny5_COFRMVtU5Z1ctipQFjnr4Y1Oe&usqp=CAU" height="100" width="120"></img>
+                
+                <br></br><br></br>
+                
                 <Link to="/add">
 
-                    <button className="btn btn-primary ">ADD NEW ITEMS</button><br></br>
+                    <button className="btn btn-danger ">ADD NEW ITEMS</button>
 
-
-                </Link><br></br><br></br><br></br><br></br>
-
-
+                </Link>
+                
                 <Link to="/viewItems">
 
                     <button className="btn btn-primary">VIEW ALL ITEMS</button>
 
 
                 </Link>
+                
+                </center>
             </div>
         )
     }
