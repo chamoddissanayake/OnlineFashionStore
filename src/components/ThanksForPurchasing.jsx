587b0471454c419abee6612430a1e829f8aa1a8a
chamoddissanayake <dissanayakechamod@gmail.com>
2020-05-16 17:46:04 +0530

    Thanks Styling

--- src/components/ThanksForPurchasing.jsx
+++ src/components/ThanksForPurchasing.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import commentBoxStyles from '../css/thanksStyles.css';
 
 export default class ThanksForPurchasing extends Component {
 
@@ -11,11 +12,33 @@ export default class ThanksForPurchasing extends Component {
         localStorage.removeItem("cart");
     }
 
+    goBackToMain = (event) => {
+        const { location } = this.props;
+        if (location.state && location.state.from) {
+            this.props.history.push(location.state.from);
+        } else {
+            this.props.history.push('/');
+        }
+        window.location.reload();
+    };
+
     render() {
         return (
             <div>
-                Thank for purchasing
+                <div class="thanks">
+                    Thank for purchasing
+                </div>
+
+
+
+                <br /><br /><br /><br /><br />
+                <div class="footerNavWrap clearfix">
+                    <div class="btn btn-success pull-left btn-fyi" onClick={this.goBackToMain} ><span class="glyphicon glyphicon-chevron-left"></span> CONTINUE SHOPPING</div>
+                    {/* <div class="btn btn-success pull-right btn-fyi">CHECKOUT<span class="glyphicon glyphicon-chevron-right"></span></div> */}
+                </div>
             </div>
         )
+
+
     }
 }
