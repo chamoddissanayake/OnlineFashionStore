054563645eb7234bf32b8dacacb4a3a2ff07fac3
chamoddissanayake <dissanayakechamod@gmail.com>
2020-05-16 09:44:45 +0530

    Firebase Connectivity Reconfigure

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 import firebase from 'firebase';
 
- const config = {
-    apiKey: "AIzaSyBhjyPRlKefR51yAFcgnSBVzouW6psm2xM",
-    authDomain: "image-upload-92fbc.firebaseapp.com",
-    databaseURL: "https://image-upload-92fbc.firebaseio.com",
-    projectId: "image-upload-92fbc",
-    storageBucket: "image-upload-92fbc.appspot.com",
-    messagingSenderId: "903474501920",
-    appId: "1:903474501920:web:ea5a3b377f773a51401f45",
-    measurementId: "G-LZLDH9RY3G"
- }
+const config = {
+   apiKey: "AIzaSyDVgjSTYMPk5U96WM2LJ3H60ZaJhfvaE9M",
+   authDomain: "fashionstore-276105.firebaseapp.com",
+   databaseURL: "https://fashionstore-276105.firebaseio.com",
+   projectId: "fashionstore-276105",
+   storageBucket: "fashionstore-276105.appspot.com",
+   messagingSenderId: "322304903819",
+   appId: "1:322304903819:web:f5e099245e805efcf0a28e",
+   measurementId: "G-HSTT06MRS6"
+}
 
- firebase.initializeApp(config);
+firebase.initializeApp(config);
 
- export default firebase
+export default firebase
