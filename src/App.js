0183be9406790308d67037c33185dcd80d11f1ec
Piyumi14 <lbpmdayarathna@gmail.com>
2020-05-23 14:15:03 +0530

    Add logo to folder

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ class App extends Component {
 
             <div className="container">
 
-              <div className="navbar-brand" to="/">Online Fashion Store</div>
+              <div className="navbar-brand" to="/">E Fashion - Online Fashion Store</div>
 
               <button className="navbar-toggler" type="button"
 
